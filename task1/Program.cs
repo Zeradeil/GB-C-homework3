da3e308152210f4fb97f558b113741b5db448630
Zeradeil <wandark@ya.ru>
2023-04-02 23:40:44 +0700

    Задание 1 на половину

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,4 +4,32 @@
 // 23432 -> да
 // 12821 -> да
 
+int ReadNum(string massage)
+{
+    System.Console.Write($"{massage} => ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+int Count(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        int num = number % 10;
+    }
+    return count;
+}
 
+int number = ReadNum("Введите число");
+int count = Count(number);
+System.Console.WriteLine(count);
+
+for (int i = 0; i < count / 2; i++)
+{
+    int firstNumber = number / 10 ^ (count - i);
+    int lastNumber = number % 10 ^ (1 + i);
+    if (firstNumber !=  lastNumber){
+        System.Console.WriteLine($"Число {number} не полиндром");
+        break;
+    }
+}
