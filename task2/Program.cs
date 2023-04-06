1f828935c83405353fc3921c8a4598e29f44f400
Zeradeil <ivanosh0@bk.ru>
2023-04-06 13:25:46 +0700

    решенное 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,5 +3,20 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double ReadNum(string massage)
+{
+    System.Console.Write($"{massage}");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+System.Console.WriteLine("Введите координаты числа A");
+double ax = ReadNum("X => ");
+double ay = ReadNum("Y => ");
+double az = ReadNum("Z => ");
+System.Console.WriteLine("Введите координаты числа B");
+double bx = ReadNum("X => ");
+double by = ReadNum("Y => ");
+double bz = ReadNum("Z => ");
 
+double d = Math.Round(Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2)), 2);
+System.Console.WriteLine(d);
 
