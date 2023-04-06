3250adec39c7a40b157af949ba1db8bc423eb454
Zeradeil <ivanosh0@bk.ru>
2023-04-06 13:48:44 +0700

    решенное 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -5,3 +5,23 @@
 // 3 -> 1, 8, 27 
 // 5 -> 1, 8, 27, 64, 125
 
+int ReadNum(string massage)
+{
+    System.Console.Write($"{massage} => ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int number = ReadNum("Введите число");
+System.Console.Write($"{number} -> ");
+
+for (int i = 1; i <= number; i++)
+{
+    if (i < number)
+    {
+        System.Console.Write($"{Math.Pow(i, 3) + ", "}");
+    }
+    else{
+        System.Console.Write($"{Math.Pow(i, 3)}");
+    }
+}
+
